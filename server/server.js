545cfb50b5bc430a11fb0ac81318aca8a84850c6
Mrbabet <wieliczkokamil@gmail.com>
2024-01-29 21:40:33 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -42,21 +42,18 @@ const generateEmailContent = (data) => {
   };
 };
 
-app.post(
-  "https://contact-form-server.onrender.com/api/sendemail",
-  async (req, res) => {
-    const data = req.body;
+app.post("/api/sendemail", async (req, res) => {
+  const data = req.body;
 
-    try {
-      await transporter.sendMail({
-        ...options,
-        ...generateEmailContent(data),
-        subject: data.subject,
-      });
+  try {
+    await transporter.sendMail({
+      ...options,
+      ...generateEmailContent(data),
+      subject: data.subject,
+    });
 
-      return res.status(200).json({ success: true });
-    } catch (err) {
-      return res.status(400).json({ message: err.message });
-    }
+    return res.status(200).json({ success: true });
+  } catch (err) {
+    return res.status(400).json({ message: err.message });
   }
-);
+});
